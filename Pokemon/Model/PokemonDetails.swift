36f0d7bec481354b964e90a111b390aff8cd4b7b
undertaker28 <pavel.starovoytovvv@gmail.com>
2023-03-10 22:35:22 +0300

    Add models

---
+++ Pokemon/Model/PokemonDetails.swift
@@ -0,0 +1,34 @@
+//
+//  PokemonDetails.swift
+//  Pokemon
+//
+//  Created by Pavel on 10.03.23.
+//
+
+// URL: https://pokeapi.co/api/v2/pokemon/{id}/
+
+struct PokemonDetails: Codable {
+    let name: String?
+    let height: Int?
+    let weight: Int?
+    let types: [TypeElement]?
+    let sprites: Sprites?
+}
+
+struct TypeElement: Codable {
+    let slot: Int?
+    let type: Species?
+}
+
+struct Species: Codable {
+    let name: String?
+    let url: String?
+}
+
+struct Sprites: Codable {
+    let frontDefault: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+    }
+}
---
+++ Pokemon/Model/PokemonList.swift
@@ -0,0 +1,21 @@
+//
+//  PokemonList.swift
+//  Pokemon
+//
+//  Created by Pavel on 10.03.23.
+//
+
+// URL 1st page: https://pokeapi.co/api/v2/pokemon
+// URL 2nd page: https://pokeapi.co/api/v2/pokemon?offset=20&limit=20
+
+struct PokemonList: Codable {
+    let count: Int
+    let next: String?
+    let previous: String?
+    let results: [PokemonInfo]
+}
+
+struct PokemonInfo: Codable {
+    let name: String
+    let url: String
+}
