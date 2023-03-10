ab80ec0b755d13d52527f659e88249c8cac85dde
undertaker28 <pavel.starovoytovvv@gmail.com>
2023-03-10 21:53:10 +0300

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Pavel on 10.03.23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
